f5844d9880e7cad6d2c6e2780b4460d2c56e1e5a
ChangYu Wang <changyu.wang1998@gmail.com>
2020-07-29 18:12:44 +1000

    Changed all variable types to numerical. Yet to standardise

--- data-wrangling.R
+++ data-wrangling.R
@@ -2,28 +2,39 @@
 
 library(xlsx)
 cad.df <- read.xlsx("Z-Alizadeh sani dataset.xlsx", 1, header=TRUE)
+cad.df.og = cad.df #copy for comparison, to be deleted after
 #This data set has no NA's or missing values.
 
 head(cad.df)
-#Here we see that there are some boolean feature to be changed to 0 and 1, as well as one categorical feature (Function.Class)
+#Here we see that there are some categorical data. We create dummy variables for them all.
 cad.df$Function.Class1 <- as.numeric(cad.df$Function.Class == 1)
 cad.df$Function.Class2 <- as.numeric(cad.df$Function.Class == 2)
 cad.df$Function.Class3 <- as.numeric(cad.df$Function.Class == 3)
 cad.df$Function.Class <- as.numeric(cad.df$Function.Class == 0)
 
+cad.df$LBBB <- as.numeric(cad.df$BBB == "LBBB")
+cad.df$RBBB <- as.numeric(cad.df$BBB == "RBBB")
 
-for(i in colnames(cad.df)) ##TODO: this works for indexing via $ as it is a char, but not for indexing using [] as it is a dataframe.
+cad.df$VHD.Mild <- as.numeric(cad.df$VHD == 'mild')
+cad.df$VHD.Moderate <- as.numeric(cad.df$VHD == 'Moderate')
+cad.df$VHD.Severe <- as.numeric(cad.df$VHD == 'Severe')
+
+#Remove old variables, as well as Exertional.CP, as all entries are N
+cad.df = subset(cad.df, select=-c(Exertional.CP,VHD,BBB))
+
+#Here we deal with changing "Y" "N" to 0 and 1.
+for(i in colnames(cad.df))
 {
-  if(isTRUE(all.equal(as.vector(range(cad.df[,i])), c("N","Y"))))
+  datarange = as.vector(unique(cad.df[,i]))
+  if(isTRUE(all.equal(datarange, c("N","Y")))|isTRUE(all.equal(datarange, c("Y","N"))))
   {
-    print(i)
     cad.df[,i] <- ifelse(cad.df[,i]=='Y',1,0)
   }
 }
 
-#Now we have to deal with BBB, and the categorical VHD.
+#Finally change the supervised variable (Cath) from "Cad" "Normal" to 1 0, respectively
+cad.df$Cath <- ifelse(cad.df$Cath =='Cad',1,0)
 
-#Finally we can normalise the data
-
+#Finally we can normalise the data, as some formulas are optimised for euclidian distance
 
 
