dbc96d50683b314a312793d947bc07a6516a11df
ChangYu Wang <changyu.wang1998@gmail.com>
2020-08-13 17:34:05 +1000

    Wrote LDA code, but LDA is probably not a good idea as it assumes a gaussian prior. Maybe avoid for this dataset.

---
+++ lda.R
@@ -0,0 +1,22 @@
+#LDA and QDA assume Gaussian prior on all features. Does not really work given our data, 
+#would not be a good idea. Can apply to a subset of the data but we will avoid for now.
+library(MASS)
+rm(list=ls())
+load(file = "caddata.RData")
+
+#Standardise non-boolean features
+df1 <- as.data.frame(lapply(cad.df, function(x) if(is.numeric(x)){
+  scale(x, center=TRUE, scale=TRUE)
+} else x))
+
+#QDA gives error Error in qda.default(x, grouping, ...) : some group is too small for 'qda'
+#need to feature select
+
+set.seed(101)
+test.idx <- sample.int(n = nrow(df1), size = floor(0.30*nrow(df1)), replace = F)
+
+lda.model = lda(Cath~., data = df1[-test.idx,])
+lda.pred = predict(lda.model, df1[test.idx,])
+
+table(pred = lda.pred$class,true = df1[test.idx,]$Cath)
+#svm.model$coefs
--- svm.R
+++ svm.R
@@ -7,12 +7,13 @@ load(file = "caddata.RData")
 df1 <- as.data.frame(lapply(cad.df, function(x) if(is.numeric(x)){
   scale(x, center=TRUE, scale=TRUE)
   } else x))
+df1 <- cad.df
 
-set.seed(7)
+set.seed(101)
 test.idx <- sample.int(n = nrow(df1), size = floor(0.30*nrow(df1)), replace = F)
 
-svm.model = svm(Cath~., data = df1[-test.idx,], kernal = "radial",cost = 1000000000, gamma = 1)
+svm.model = svm(Cath~., data = df1[-test.idx,], kernal = "radial",cost = 0.1, gamma = 0.1)
 svm.pred = predict(svm.model, df1[test.idx,])
 
-table(pred = svm.pred, true = df1[test.idx,]$Cath)
-svm.model$coefs
+table(pred = svm.pred, true = df1[test.idx,]$Cath) #this does not work yet. i am confused
+#svm.model$coefs
