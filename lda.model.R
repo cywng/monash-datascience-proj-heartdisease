824388a970e0a58735e53a77af35ae2a3b0f20fd
ChangYu Wang <changyu.wang1998@gmail.com>
2020-08-17 17:55:35 +1000

    Updated LDA to select features AFTER train test split.

--- lda.model.R
+++ lda.model.R
@@ -1,6 +1,8 @@
 #LDA assumes Gaussian prior on all features. Does not really work given our data, 
 #would not be a good idea. Can apply to a subset of the data but we will avoid for now.
 #resource: https://rpubs.com/maulikpatel/229684
+#We should feature select AFTER train/test split. proof:https://stackoverflow.com/questions/56308116/should-feature-selection-be-done-before-train-test-split-or-after#:~:text=The%20contradicting%20answer%20is%20that,in%20random_state%20of%20the%20Train_Test_Split.
+
 library(MASS)
 library(caret)
 rm(list=ls())
@@ -21,26 +23,26 @@ test.df = df1[-index,]
 
 #Feature selection using Recursive feature elimination on LDA - converted  
 control <- rfeControl(functions=ldaFuncs, method="cv")
-lda.features <- rfe(df1[,names(df1) != c("Cath")], df1$Cath, sizes=c(1:60), rfeControl=control) #takes a long time, care
+lda.features <- rfe(train.df[,names(df1) != c("Cath")], train.df$Cath, sizes=c(1:60), rfeControl=control) #takes a long time, care
 plot(lda.features, type=c("g", "o"))
 predictors(lda.features)
-#20 features selected
-#[1] "Typical.Chest.Pain" "Age"                "Atypical"           "EF.TTE"            
-#[5] "HTN"                "FBS"                "ESR"                "DM"                
-#[9] "Tinversion"         "TG"                 "K"                  "PR"                
-#[13] "Lymph"              "Neut"               "VHD.Mild"           "Dyspnea"           
-#[17] "St.Depression"      "Nonanginal"         "Region.RWMA2"       "Na"     
-#[21] "BUN" 
+#21 features selected
+#[1] "Typical.Chest.Pain" "Atypical"           "Age"                "EF.TTE"            
+#[5] "FBS"                "HTN"                "ESR"                "DM"                
+#[9] "Tinversion"         "K"                  "TG"                 "PR"                
+#[13] "Neut"               "Nonanginal"         "Lymph"              "Na"                
+#[17] "St.Depression"      "VHD.Mild"           "Region.RWMA2"       "Dyspnea"           
+#[21] "PLT"   
 
 
 #Train and test the model
 lda.model = train(train.df[lda.features$optVariables], train.df$Cath, method="lda", trControl = trainControl(method = "cv"))
 lda.pred = predict(lda.model, test.df[lda.features$optVariables])
 table(pred = lda.pred, true = test.df$Cath) 
-#table:true  (test accuracy .85, train accuracy .88)
+#table:true  (test accuracy .90, train accuracy .88
 #pred  0  1
-#0    12  4
-#1     5 40
+#0    14  3
+#1     3 41
 
 #Save resultant model
 save(lda.model,lda.features, file = "LDAmodel.RData")
