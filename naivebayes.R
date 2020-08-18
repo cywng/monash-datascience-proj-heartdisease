d0e6d245cfa332de4edb7737b664613d4a7281e7
123saaa <39576420+123saaa@users.noreply.github.com>
2020-08-18 16:23:17 +1000

    Naive Bayes

---
+++ naivebayes.R
@@ -0,0 +1,30 @@
+library(caret)
+library(rsample)
+library(klaR)
+
+rm(list=ls())
+load(file = "/Users/wangyunxuan/Downloads/caddata (3).RData")
+df=as.data.frame(cad.df.balanced)
+#head(df)
+#which( colnames(df)=="Cath" )
+#n<-ncol(df)
+
+set.seed(123)
+rcontrol <- rfeControl(functions = nbFuncs,method="repeatedcv")#, repeats=10,verbose = FALSE)
+result<-rfe(x=df[,c(1:42,44:54)],y=df[,43],sizes = c(1:54),rfeControl =rcontrol)
+print(result)
+predictors(result)
+result$fit
+
+split<-initial_split(df[,c(predictors(result),"Cath")] ,prop = .8)
+train <- training(split)
+test  <- testing(split)
+
+
+
+control <- trainControl(method="repeatedcv", number=10)
+train_model<-train(Cath ~., data = train, method="nb", ,trControl=control)
+train_model$results
+pred=predict(train_model,test)
+mean(pred== test$Cath)
+
