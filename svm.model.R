e88ff88f76dc084c066ebea3d2ac499c2fb7ffcc
ChangYu Wang <changyu.wang1998@gmail.com>
2020-08-20 18:06:17 +1000

    tried feature selection but no SVM case. might do manually or use tree based methods.

--- svm.model.R
+++ svm.model.R
@@ -1,29 +1,37 @@
 library(caret)
-libraty(e1071)
+library(e1071)
 rm(list=ls())
 load(file = "caddata.RData")
+df1 <- cad.df.balanced
 #Source https://neerajkumar.org/writings/svm/#:~:text=Prescaling%2Fnormalization%2Fwhitening,are%20different%20types%20of%20whitening.)
 
-#Standardise non-boolean features
-#df1 <- as.data.frame(lapply(cad.df, function(x) if(is.numeric(x)){
-#  scale(x, center=TRUE, scale=TRUE)
-#  } else x))
-#df1 <- cad.df
-
 set.seed(101)
-train_control <- trainControl(method="repeatedcv", number=10, repeats=10)
+index <- sample.int(n = nrow(df1), size = floor(0.80*nrow(df1)), replace = F) #use caret
+train.df = df1[index,]
+test.df = df1[-index,]
 
-svm.model <- train(Cath ~., data = cad.df.balanced, method = "svmRadial", trControl = train_control,  preProcess = c("center","scale"), tuneLength = 10)
+
+#Feature selection using Recursive feature elimination 
+control <- rfeControl(functions=rfFuncs, method="cv")
+svm.features <- rfe(train.df[,names(df1) != c("Cath")], train.df$Cath, sizes=c(1:60), rfeControl=control) #takes a long time, care
+plot(svm.features, type=c("g", "o"))
+predictors(svm.features)
+#14 results,  [1] "Typical.Chest.Pain" "Atypical"           "Age"                "Nonanginal"        
+#[5] "EF.TTE"             "HTN"                "Tinversion"         "Region.RWMA2"      
+#[9] "VHD.Severe"         "DM"                 "Diastolic.Murmur"   "TG"                
+#[13] "ESR"                "FBS" 
+
+
+# Set up Repeated k-fold Cross Validation
+train_control <- trainControl(method="repeatedcv", number=10, repeats=3)
+
+svm.model <- train(Cath ~., data = train.df, method = "svmRadial", trControl = train_control,  preProcess = c("center","scale"), tuneLength = 10)
 #svm.model$bestTune radial
 #sigma      C
 #0.01182739 1
 
-test.idx <- sample.int(n = nrow(cad.df.balanced), size = floor(0.2*nrow(cad.df.balanced)), replace = F)
-svm.pred = predict(svm.model, cad.df.balanced[test.idx,])
 
-table(pred = svm.pred, true = cad.df.balanced[test.idx,]$Cath) 
-#svm.model$coefs
-importance <- varImp(svm.model, scale=FALSE)
-# summarize importance
-print(importance)
-plot(importance)
+svm.pred = predict(svm.model, test.df)
+
+table(pred = svm.pred, true = test.df$Cath) 
+
