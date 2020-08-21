f685b5f5b7c5e9527e9de7729bdfbc952666c627
ChangYu Wang <changyu.wang1998@gmail.com>
2020-08-21 18:32:58 +1000

    Wrote new code running RFE 4 times using 4 methods to select strongest variables. Sizes ranged from 11-21

---
+++ featureselectionattempts.R
@@ -0,0 +1,51 @@
+#Testing a bunch of feature selection algs.
+library(caret)
+library(klaR)
+rm(list=ls())
+set.seed(123)
+load(file = "caddata.RData")
+
+#convert factors to numerical again because LDA does not like factors.
+df1 <- as.data.frame(lapply(cad.df.balanced, function(x) if(is.factor(x)){
+  as.numeric(x)-1
+} else x))
+df1$Cath <- as.factor(df1$Cath)
+
+#Separate into training and test data
+index <- sample.int(n = nrow(df1), size = floor(0.80*nrow(df1)), replace = F) #use caret
+train.df = df1[index,]
+test.df = df1[-index,]
+
+#====LDA====
+control <- rfeControl(functions=ldaFuncs, method="cv")
+lda.features <- rfe(train.df[,names(df1) != c("Cath")], train.df$Cath, sizes=c(1:30), rfeControl=control) 
+
+#====Logistic Regression====
+control <- rfeControl(functions=lrFuncs, method="cv")
+lr.features <- rfe(train.df[,names(df1) != c("Cath")], train.df$Cath, sizes=c(1:30), rfeControl=control) 
+
+#====RandomForest====
+control <- rfeControl(functions=rfFuncs, method="cv")
+rf.features <- rfe(train.df[,names(df1) != c("Cath")], train.df$Cath, sizes=c(1:30), rfeControl=control)
+
+#====Naive Bayes====
+control <- rfeControl(functions=nbFuncs, method="cv")
+nb.features <- rfe(train.df[,names(df1) != c("Cath")], train.df$Cath, sizes=c(1:30), rfeControl=control)
+
+#====Plot all results====
+p1 <- plot(lda.features, type=c("g", "o"),main="LDA")
+p2 <- plot(lr.features, type=c("g", "o"),main="LogisticRegression")
+p3 <- plot(rf.features, type=c("g", "o"),main="RandForest")
+p4 <- plot(nb.features, type=c("g", "o"),main="NaiveBayes")
+
+print(p1, split=c(1,1,2,2), more=TRUE)
+print(p2, split=c(1,2,2,2), more=TRUE)
+print(p3, split=c(2,1,2,2), more=TRUE)
+print(p4, split=c(2,2,2,2), more=TRUE)
+
+#====Check all for best within 3% accuracy.
+
+pickSizeTolerance(lda.features$results,metric= "Accuracy",maximize = TRUE, tol = 1)
+pickSizeTolerance(lr.features$results,metric= "Accuracy",maximize = TRUE, tol = 1)
+pickSizeTolerance(rf.features$results,metric= "Accuracy",maximize = TRUE, tol = 1)
+pickSizeTolerance(nb.features$results,metric= "Accuracy",maximize = TRUE, tol = 1)
