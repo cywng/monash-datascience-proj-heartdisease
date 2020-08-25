5df76eb02eb9945ddd401c37eb6cd818af47241e
王蕴璇 <wangyunxuan@dyn-118-138-112-118.its.monash.edu.au>
2020-08-25 16:48:17 +1000

    Naive Bayes

--- Naive Bayes.R
+++ Naive Bayes.R
@@ -5,11 +5,6 @@ library(klaR)
 rm(list=ls())
 load(file = "caddata.RData")
 df=as.data.frame(cad.df.balanced)
-#head(df)
-#which( colnames(df)=="Cath" )
-#n<-ncol(df)
-#c(1:54)
-#c(1:42,44:54)
 set.seed(123)
 split<-initial_split(df ,prop = .8)
 train <- training(split)
@@ -17,9 +12,8 @@ test  <- testing(split)
 
 rcontrol <- rfeControl(functions = nbFuncs,method="cv")#, repeats=10,verbose = FALSE)
 result<-rfe(x=train[,c(1:42,44:54)],y=train[,43],sizes = c(1:54),rfeControl =rcontrol)
-#print(result)
 predictors(result)
-#result$fit
+
 
 train <- train[,c(predictors(result),"Cath")]
 test  <- test[,c(predictors(result),"Cath")]
