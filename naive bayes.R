61d6f0e90db4579d295c17ac4a4e8617e840bba2
123saaa <2733015417@qq.com>
2020-08-25 17:33:10 +1000

    nb

---
+++ naive bayes.R
@@ -0,0 +1,20 @@
+library(caret)
+library(rsample)
+library(klaR)
+
+load(file = "caddata.RData")
+load(file = "Featuresselected.RData")
+df=as.data.frame(cad.df.balanced)
+
+set.seed(123)
+
+
+train <- train.df[,c(predictors(nb.features),"Cath")]
+test  <- test.df[,c(predictors(nb.features),"Cath")]
+
+
+control <- trainControl(method="repeatedcv", number=10)
+train_model<-train(Cath ~., data = train, method="nb", ,trControl=control)
+pred=predict(train_model,test)
+
+mean(pred== test$Cath)
