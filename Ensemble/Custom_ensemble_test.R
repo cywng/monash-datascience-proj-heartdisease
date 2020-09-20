4f3844bf9a5d840d2c9e610aabee63aee7f15f22
ChangYu Wang <changyu.wang1998@gmail.com>
2020-09-20 22:53:51 +1000

    Generated df to try custom model

---
+++ Ensemble/Custom_ensemble_test.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+load(file = "DataWrangling/Featuresselected.RData")
+load(file = "Models/SVMradmodel.RData")
+load(file = "Models/svmpolymodel.RData")
+load(file = "Models/svmlinmodel.RData")
+load(file = "Models/RF.RData")
+load(file = "Models/Neural_Network.RData")
+load(file = "Models/NB.RData")
+load(file = "Models/LR.RData")
+load(file = "Models/LDAmodel.RData")
+load(file = "Models/knnmodel.RData")
+
+train.df$Cath <- as.factor(ifelse(train.df$Cath == 0,"N","Y"))
+test.df$Cath <- as.factor(ifelse(test.df$Cath == 0,"N","Y"))
+
+generate_ensemble_df <- function(caddataset){
+  #models <-  c(knn.model, lda.model, LR_model, NB_model, RF_model, svmlin.model, svmpoly.model, svmrad.model, nn1) this is some fucked up thing
+  aggregate_pred.df <- as.data.frame(caddataset$Cath)
+  colnames(aggregate_pred.df)=c("Cath")
+  #attach predictions
+  aggregate_pred.df$knnres <-  predict(knn.model, caddataset[knn.features])
+  aggregate_pred.df$ldares <-  predict(lda.model, caddataset[lda.features$optVariables])
+  aggregate_pred.df$lrres <-  predict(LR_model, caddataset[lr.features$optVariables])
+  aggregate_pred.df$nbres <-  predict(NB_model, caddataset[nb.features$optVariables])
+  aggregate_pred.df$rfres <-  predict(RF_model, caddataset[rf.features$optVariables])
+  aggregate_pred.df$svmLres <-  predict(svmlin.model, caddataset[svmlin.features$optVariables])
+  aggregate_pred.df$svmPres <-  predict(svmpoly.model, caddataset[svmpoly.features$optVariables])
+  aggregate_pred.df$svmRres <-  predict(svmrad.model, caddataset[svmrad.features$optVariables])
+  aggregate_pred.df$NNres <-  predict(nn1, caddataset)
+  return(aggregate_pred.df)
+}
+
+pred.df <- generate_ensemble_df(train.df)
