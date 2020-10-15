d1863dffc80b1aa41d252cc470f1e1deb19e32ea
ChangYu Wang <changyu.wang1998@gmail.com>
2020-10-15 22:46:29 +1100

    Added some filler descriptions.

--- API/plumber_api.R
+++ API/plumber_api.R
@@ -28,11 +28,11 @@ function(req){
 #' predict Coronary Artery Disease with an LDA model.
 #' @param Typical.Chest.Pain:numeric Presence of chest pain (1 for yes, 0 for no).
 #' @param Age:numeric The age of the patient.
-#' @param Atypical:int 
-#' @param FBS:numeric Fasting blood sugar level (mg/dl, 62-400)
+#' @param Atypical:int Atypical presence (1 for yes, 0 for no).
+#' @param FBS:numeric Fasting blood sugar level (mg/dl, 62-400).
 #' @param HTN:int Presence of hypertension (1 for yes, 0 for no).
 #' @param DM:int Presence of diabetes mellitus (1 for yes, 0 for no).
-#' @param EF.TTE:numeric
+#' @param EF.TTE:numeric Level of EF.TTE (15-60).
 #' @param K:numeric Potassium content in blood (mEq/lit; 3.0-6.6).
 #' @param PR:numeric Resting heart rate (bpm; 50-110).
 #' @param ESR:numeric Erythrocyte sedimentation rate (mm/h; 1-90).
