827cdd8b6254281f0f09a95448c304ec3386762e
123saaa <2733015417@qq.com>
2020-10-20 14:40:42 +1100

    test file

--- website/OutputTest.R
+++ website/OutputTest.R
@@ -8,33 +8,83 @@ context("Test Shiny app")
 app <- shinytest::ShinyDriver$new("~/monash-datascience-proj-heartdisease/website/FIT3164team1cadprediction/")
 
 test_that("output is correct", {
-  app$setInputs("Typical.Chest.Pain"=0,"Age"=53,
-                "Atypical"="N","FBS"=90,"HTN"=1,
-                "DM"=0,"EF.TTE"=50, "K"=4.7,BP=110,ESR=7, 
-                "TG"=250,"Region.RWMA"=0)
-  #app$setInputs("Typical.Chest.Pain"=0,"Age"=53,"Atypical"=0,"FBS"=90,"HTN"=1,"DM"=0,"EF.TTE"=50, "K"=4.7,"PR"=80,"ESR"=7,"TG"=250,"Tinversion"=1,"Lymph"=39,"Neut"=52,"St.Depression"=1,"Dyspnea"=0,"Nonanginal"=0,"Region.RWMA2"=0,"VHD.Mild"=0,"PLT"=261,"BMI"=29.38,"Na"=141)
-  # get text_out
-  app$setInputs(button="click")
-  output <- app$getValue(name="pred")
-  a<-paste0("",output)
-  numextract <- function(string){ 
-    str_extract(string, "\\-*\\d+\\.*\\d*")
-  } 
-  num<-as.numeric(numextract(a))
-  if(num>=0.5){
-    result<-"Cad"
-  }else{
-    result<-"Normal"
+  i=1
+  while (i<=10){
+    #------
+    if(as.numeric(svm.train$Typical.Chest.Pain[i])==0){
+      Typical.Chest.Pain<-"No"
+    }
+    else{
+   Typical.Chest.Pain<-"Yes"
+    }
+    #-----
+    if(as.numeric(svm.train$DM[i])==0){
+           DM<-"No"
+    }
+    else{
+    DM<-"Yes"
+    }
+    #---------
+    if(as.numeric(svm.train$HTN[i])==0){
+      HTN<-"No"
+    }
+    else{
+     HTN<-"Yes"
+    }
+    #-------------
+    if(as.numeric(svm.train$Atypical[i])==0){
+    Atypical<-"No"
+    }
+    else{
+    Atypical<-"Yes"
+    }
+    data<-data.frame("Typical.Chest.Pain"=Typical.Chest.Pain,"Age"=svm.train$Age[i],
+                     "Atypical"=Atypical,"FBS"=svm.train$FBS[i],"HTN"=HTN,
+                     "DM"=DM,"EF.TTE"=svm.train$EF.TTE[i], "K"=svm.train$K[i],"BP"=svm.train$BP[i],"ESR"=svm.train$ESR[i], 
+                     "TG"=svm.train$TG[i],"Region.RWMA"=svm.train$Region.RWMA[i])
+    app$setInputs("Typical.Chest.Pain"=Typical.Chest.Pain,"Age"=svm.train$Age[i],
+                  "Atypical"=Atypical,"FBS"=svm.train$FBS[i],"HTN"=HTN,
+                  "DM"=DM,"EF.TTE"=svm.train$EF.TTE[i], "K"=svm.train$K[i],"BP"=svm.train$BP[i],"ESR"=svm.train$ESR[i], 
+                  "TG"=svm.train$TG[i],"Region.RWMA"=svm.train$Region.RWMA[i])
+    app$setInputs(button="click")
+    output <- app$getValue(name="pred")
+    a<-paste0("",output)
+    numextract <- function(string){ 
+      str_extract(string, "\\-*\\d+\\.*\\d*")
+    } 
+    num<-as.numeric(numextract(a))
+    if(num>=0.5){
+      result<-"Cad"
+    }else{
+      result<-"Normal"
+    }
+    
+    Typical.Chest.Pain<-as.factor(as.numeric(ifelse(Typical.Chest.Pain=="Yes",1,0)))
+    Atypical<-as.factor( ifelse(Atypical=="Yes","Y","N"))
+    HTN<-as.factor(as.numeric(ifelse(HTN=="Yes",1,0)))
+    DM<-as.factor(as.numeric(ifelse(DM=="Yes",1,0)))
+    data<-data.frame("Typical.Chest.Pain"=Typical.Chest.Pain,"Age"=svm.train$Age[i],
+                     "Atypical"=Atypical,"FBS"=svm.train$FBS[i],"HTN"=HTN,
+                     "DM"=DM,"EF.TTE"=svm.train$EF.TTE[i], "K"=svm.train$K[i],BP=svm.train$BP[i],ESR=svm.train$ESR[i], 
+                     "TG"=svm.train$TG[i],"Region.RWMA"=svm.train$Region.RWMA[i])
+    pr=round(predict(svm.l,data[,!names(data) %in% c("Cath")],type = "prob")$Cad,2)
+    if(pr>=0.5){
+      test<-"Cad"
+    }else{
+      test<-"Normal"
+    }
+    expect_equal(result,test) 
+    i=i+1
   }
-  a<-data.frame("Typical.Chest.Pain"=as.factor(0),"Age"=53,
-                "Atypical"="N","FBS"=90,"HTN"=as.factor(1),
-                "DM"=as.factor(0),"EF.TTE"=50, "K"=4.7,BP=110,ESR=7, 
-                "TG"=250,"Region.RWMA"=as.factor(0))
-  pred<-predict(svm.l,a)
-  a<-paste0("",pred)
-  expect_equal(result,a) 
+  
+  
+  
+  
+  #app$setInputs("Typical.Chest.Pain"=0,"Age"=53,"Atypical"=0,"FBS"=90,"HTN"=1,"DM"=0,"EF.TTE"=50, "K"=4.7,"PR"=80,"ESR"=7,"TG"=250,"Tinversion"=1,"Lymph"=39,"Neut"=52,"St.Depression"=1,"Dyspnea"=0,"Nonanginal"=0,"Region.RWMA2"=0,"VHD.Mild"=0,"PLT"=261,"BMI"=29.38,"Na"=141)
   
 }
 )
 # stop the Shiny app
 app$stop()
+
+
